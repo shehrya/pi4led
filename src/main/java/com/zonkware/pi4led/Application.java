5fb67bdb928c3145b9edfa9cf89da76ec708d47f
shehrya <khanshehryar362@gmail.com>
2017-07-10 17:15:23 +0200

    program

---
+++ src/main/java/com/zonkware/pi4led/Application.java
@@ -0,0 +1,20 @@
+package com.zonkware.pi4led;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+
+import javax.swing.*;
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args){
+
+        SpringApplication.run(Application.class, args);
+
+
+
+    }
+
+}
